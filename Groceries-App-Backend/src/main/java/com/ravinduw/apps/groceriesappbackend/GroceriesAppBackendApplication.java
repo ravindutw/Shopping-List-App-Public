9a5bee10582a9ae7c56c70b8c894adc253c431ee
IT24101979 <it24101979@my.sliit.lk>
2025-06-22 18:18:06 +0530

    Initial commit

---
+++ Groceries-App-Backend/src/main/java/com/ravinduw/apps/groceriesappbackend/GroceriesAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ravinduw.apps.groceriesappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceriesAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceriesAppBackendApplication.class, args);
+    }
+
+}
---
+++ Groceries-App-Backend/src/test/java/com/ravinduw/apps/groceriesappbackend/GroceriesAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravinduw.apps.groceriesappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceriesAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
